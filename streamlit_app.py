7bde0487338410be78c5d3660174c92fb34c3c1b
mhall97 <163345823+mhall97@users.noreply.github.com>
2024-03-13 23:24:45 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,10 +34,9 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-	search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-	st.write('The serach value for ' , fruit_chose, ' is ' , search_on, '.')
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The serach value for ' , fruit_chosen, ' is ' , search_on, '.')
 
-		
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width = True)
@@ -48,8 +47,3 @@ if ingredients_list:
     """
 
     time_to_insert = st.button('Submit Order')
-
-    if time_to_insert:
-        session.sql(my_insrt_stmt).collect()
-	    
-        st.success('Your Smoothie is ordered! ' + name_on_order, icon="✅")
